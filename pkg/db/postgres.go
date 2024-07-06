c53dd36a781a88a982c3e54441bb498926d9b694
m2k6artworks <zidangibran14@gmail.com>
2024-07-06 16:00:59 +0700

    initialize & setup db connection

---
+++ pkg/db/postgres.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+)
+
+func OpenDB() (*sql.DB, error) {
+	var db *sql.DB
+
+	port, err := strconv.Atoi("5432")
+	if err != nil {
+		return db, err
+	}
+
+	return sql.Open("postgres",
+		fmt.Sprintf(
+			"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			"localhost", port, "postgres", "pass", "unsia_go",
+		),
+	)
+
+	return sql.Open("postgres", "root:pass@tcp(localhost:3306)/essentials?parseTime=true")
+}

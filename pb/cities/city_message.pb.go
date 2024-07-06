2d4fc8d5a4e4e01a28944e315c2246f3a7ab0106
m2k6artworks <zidangibran14@gmail.com>
2024-07-07 00:00:59 +0700

    setup grpc

--- pb/cities/city_message.pb.go
+++ pb/cities/city_message.pb.go
@@ -75,6 +75,232 @@ func (x *City) GetName() string {
 	return ""
 }
 
+type EmptyMessage struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *EmptyMessage) Reset() {
+	*x = EmptyMessage{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_city_message_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *EmptyMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*EmptyMessage) ProtoMessage() {}
+
+func (x *EmptyMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_city_message_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use EmptyMessage.ProtoReflect.Descriptor instead.
+func (*EmptyMessage) Descriptor() ([]byte, []int) {
+	return file_city_message_proto_rawDescGZIP(), []int{1}
+}
+
+type CitiesStream struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	City *City `protobuf:"bytes,1,opt,name=city,proto3" json:"city,omitempty"`
+}
+
+func (x *CitiesStream) Reset() {
+	*x = CitiesStream{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_city_message_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CitiesStream) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CitiesStream) ProtoMessage() {}
+
+func (x *CitiesStream) ProtoReflect() protoreflect.Message {
+	mi := &file_city_message_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CitiesStream.ProtoReflect.Descriptor instead.
+func (*CitiesStream) Descriptor() ([]byte, []int) {
+	return file_city_message_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *CitiesStream) GetCity() *City {
+	if x != nil {
+		return x.City
+	}
+	return nil
+}
+
+type Id struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id int32 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (x *Id) Reset() {
+	*x = Id{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_city_message_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Id) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Id) ProtoMessage() {}
+
+func (x *Id) ProtoReflect() protoreflect.Message {
+	mi := &file_city_message_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Id.ProtoReflect.Descriptor instead.
+func (*Id) Descriptor() ([]byte, []int) {
+	return file_city_message_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *Id) GetId() int32 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
+type CityInput struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+}
+
+func (x *CityInput) Reset() {
+	*x = CityInput{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_city_message_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CityInput) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CityInput) ProtoMessage() {}
+
+func (x *CityInput) ProtoReflect() protoreflect.Message {
+	mi := &file_city_message_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CityInput.ProtoReflect.Descriptor instead.
+func (*CityInput) Descriptor() ([]byte, []int) {
+	return file_city_message_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *CityInput) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+type MyBoolean struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Boolean bool `protobuf:"varint,1,opt,name=boolean,proto3" json:"boolean,omitempty"`
+}
+
+func (x *MyBoolean) Reset() {
+	*x = MyBoolean{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_city_message_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *MyBoolean) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*MyBoolean) ProtoMessage() {}
+
+func (x *MyBoolean) ProtoReflect() protoreflect.Message {
+	mi := &file_city_message_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use MyBoolean.ProtoReflect.Descriptor instead.
+func (*MyBoolean) Descriptor() ([]byte, []int) {
+	return file_city_message_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *MyBoolean) GetBoolean() bool {
+	if x != nil {
+		return x.Boolean
+	}
+	return false
+}
+
 var File_city_message_proto protoreflect.FileDescriptor
 
 var file_city_message_proto_rawDesc = []byte{
@@ -82,7 +308,17 @@ var file_city_message_proto_rawDesc = []byte{
 	0x72, 0x6f, 0x74, 0x6f, 0x12, 0x06, 0x63, 0x69, 0x74, 0x69, 0x65, 0x73, 0x22, 0x2a, 0x0a, 0x04,
 	0x43, 0x69, 0x74, 0x79, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05,
 	0x52, 0x02, 0x69, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01,
-	0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x12, 0x5a, 0x10, 0x70, 0x62, 0x2f, 0x63,
+	0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x22, 0x0e, 0x0a, 0x0c, 0x45, 0x6d, 0x70, 0x74,
+	0x79, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x30, 0x0a, 0x0c, 0x43, 0x69, 0x74, 0x69,
+	0x65, 0x73, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x20, 0x0a, 0x04, 0x63, 0x69, 0x74, 0x79,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x0c, 0x2e, 0x63, 0x69, 0x74, 0x69, 0x65, 0x73, 0x2e,
+	0x43, 0x69, 0x74, 0x79, 0x52, 0x04, 0x63, 0x69, 0x74, 0x79, 0x22, 0x14, 0x0a, 0x02, 0x49, 0x64,
+	0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64,
+	0x22, 0x1f, 0x0a, 0x09, 0x43, 0x69, 0x74, 0x79, 0x49, 0x6e, 0x70, 0x75, 0x74, 0x12, 0x12, 0x0a,
+	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d,
+	0x65, 0x22, 0x25, 0x0a, 0x09, 0x4d, 0x79, 0x42, 0x6f, 0x6f, 0x6c, 0x65, 0x61, 0x6e, 0x12, 0x18,
+	0x0a, 0x07, 0x62, 0x6f, 0x6f, 0x6c, 0x65, 0x61, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52,
+	0x07, 0x62, 0x6f, 0x6f, 0x6c, 0x65, 0x61, 0x6e, 0x42, 0x12, 0x5a, 0x10, 0x70, 0x62, 0x2f, 0x63,
 	0x69, 0x74, 0x69, 0x65, 0x73, 0x3b, 0x63, 0x69, 0x74, 0x69, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72,
 	0x6f, 0x74, 0x6f, 0x33,
 }
@@ -99,16 +335,22 @@ func file_city_message_proto_rawDescGZIP() []byte {
 	return file_city_message_proto_rawDescData
 }
 
-var file_city_message_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_city_message_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_city_message_proto_goTypes = []any{
-	(*City)(nil), // 0: cities.City
+	(*City)(nil),         // 0: cities.City
+	(*EmptyMessage)(nil), // 1: cities.EmptyMessage
+	(*CitiesStream)(nil), // 2: cities.CitiesStream
+	(*Id)(nil),           // 3: cities.Id
+	(*CityInput)(nil),    // 4: cities.CityInput
+	(*MyBoolean)(nil),    // 5: cities.MyBoolean
 }
 var file_city_message_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: cities.CitiesStream.city:type_name -> cities.City
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_city_message_proto_init() }
@@ -129,6 +371,66 @@ func file_city_message_proto_init() {
 				return nil
 			}
 		}
+		file_city_message_proto_msgTypes[1].Exporter = func(v any, i int) any {
+			switch v := v.(*EmptyMessage); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_city_message_proto_msgTypes[2].Exporter = func(v any, i int) any {
+			switch v := v.(*CitiesStream); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_city_message_proto_msgTypes[3].Exporter = func(v any, i int) any {
+			switch v := v.(*Id); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_city_message_proto_msgTypes[4].Exporter = func(v any, i int) any {
+			switch v := v.(*CityInput); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_city_message_proto_msgTypes[5].Exporter = func(v any, i int) any {
+			switch v := v.(*MyBoolean); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -136,7 +438,7 @@ func file_city_message_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_city_message_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- pb/cities/city_service_grpc.pb.go
+++ pb/cities/city_service_grpc.pb.go
@@ -19,14 +19,22 @@ import (
 const _ = grpc.SupportPackageIsVersion8
 
 const (
-	CitiesService_GetCity_FullMethodName = "/cities.CitiesService/GetCity"
+	CitiesService_GetCity_FullMethodName   = "/cities.CitiesService/GetCity"
+	CitiesService_GetCities_FullMethodName = "/cities.CitiesService/GetCities"
+	CitiesService_Create_FullMethodName    = "/cities.CitiesService/Create"
+	CitiesService_Update_FullMethodName    = "/cities.CitiesService/Update"
+	CitiesService_Delete_FullMethodName    = "/cities.CitiesService/Delete"
 )
 
 // CitiesServiceClient is the client API for CitiesService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type CitiesServiceClient interface {
-	GetCity(ctx context.Context, in *City, opts ...grpc.CallOption) (*City, error)
+	GetCity(ctx context.Context, in *Id, opts ...grpc.CallOption) (*City, error)
+	GetCities(ctx context.Context, in *EmptyMessage, opts ...grpc.CallOption) (CitiesService_GetCitiesClient, error)
+	Create(ctx context.Context, in *CityInput, opts ...grpc.CallOption) (*City, error)
+	Update(ctx context.Context, in *City, opts ...grpc.CallOption) (*City, error)
+	Delete(ctx context.Context, in *Id, opts ...grpc.CallOption) (*MyBoolean, error)
 }
 
 type citiesServiceClient struct {
@@ -37,10 +45,73 @@ func NewCitiesServiceClient(cc grpc.ClientConnInterface) CitiesServiceClient {
 	return &citiesServiceClient{cc}
 }
 
-func (c *citiesServiceClient) GetCity(ctx context.Context, in *City, opts ...grpc.CallOption) (*City, error) {
+func (c *citiesServiceClient) GetCity(ctx context.Context, in *Id, opts ...grpc.CallOption) (*City, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(City)
 	err := c.cc.Invoke(ctx, CitiesService_GetCity_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *citiesServiceClient) GetCities(ctx context.Context, in *EmptyMessage, opts ...grpc.CallOption) (CitiesService_GetCitiesClient, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	stream, err := c.cc.NewStream(ctx, &CitiesService_ServiceDesc.Streams[0], CitiesService_GetCities_FullMethodName, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &citiesServiceGetCitiesClient{ClientStream: stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type CitiesService_GetCitiesClient interface {
+	Recv() (*CitiesStream, error)
+	grpc.ClientStream
+}
+
+type citiesServiceGetCitiesClient struct {
+	grpc.ClientStream
+}
+
+func (x *citiesServiceGetCitiesClient) Recv() (*CitiesStream, error) {
+	m := new(CitiesStream)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *citiesServiceClient) Create(ctx context.Context, in *CityInput, opts ...grpc.CallOption) (*City, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(City)
+	err := c.cc.Invoke(ctx, CitiesService_Create_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *citiesServiceClient) Update(ctx context.Context, in *City, opts ...grpc.CallOption) (*City, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(City)
+	err := c.cc.Invoke(ctx, CitiesService_Update_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *citiesServiceClient) Delete(ctx context.Context, in *Id, opts ...grpc.CallOption) (*MyBoolean, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(MyBoolean)
+	err := c.cc.Invoke(ctx, CitiesService_Delete_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -51,15 +122,31 @@ func (c *citiesServiceClient) GetCity(ctx context.Context, in *City, opts ...grp
 // All implementations should embed UnimplementedCitiesServiceServer
 // for forward compatibility
 type CitiesServiceServer interface {
-	GetCity(context.Context, *City) (*City, error)
+	GetCity(context.Context, *Id) (*City, error)
+	GetCities(*EmptyMessage, CitiesService_GetCitiesServer) error
+	Create(context.Context, *CityInput) (*City, error)
+	Update(context.Context, *City) (*City, error)
+	Delete(context.Context, *Id) (*MyBoolean, error)
 }
 
 // UnimplementedCitiesServiceServer should be embedded to have forward compatible implementations.
 type UnimplementedCitiesServiceServer struct {
 }
 
-func (UnimplementedCitiesServiceServer) GetCity(context.Context, *City) (*City, error) {
+func (UnimplementedCitiesServiceServer) GetCity(context.Context, *Id) (*City, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetCity not implemented")
+}
+func (UnimplementedCitiesServiceServer) GetCities(*EmptyMessage, CitiesService_GetCitiesServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetCities not implemented")
+}
+func (UnimplementedCitiesServiceServer) Create(context.Context, *CityInput) (*City, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (UnimplementedCitiesServiceServer) Update(context.Context, *City) (*City, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
+}
+func (UnimplementedCitiesServiceServer) Delete(context.Context, *Id) (*MyBoolean, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
 
 // UnsafeCitiesServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -74,7 +161,7 @@ func RegisterCitiesServiceServer(s grpc.ServiceRegistrar, srv CitiesServiceServe
 }
 
 func _CitiesService_GetCity_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(City)
+	in := new(Id)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -86,7 +173,82 @@ func _CitiesService_GetCity_Handler(srv interface{}, ctx context.Context, dec fu
 		FullMethod: CitiesService_GetCity_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(CitiesServiceServer).GetCity(ctx, req.(*City))
+		return srv.(CitiesServiceServer).GetCity(ctx, req.(*Id))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CitiesService_GetCities_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(EmptyMessage)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(CitiesServiceServer).GetCities(m, &citiesServiceGetCitiesServer{ServerStream: stream})
+}
+
+type CitiesService_GetCitiesServer interface {
+	Send(*CitiesStream) error
+	grpc.ServerStream
+}
+
+type citiesServiceGetCitiesServer struct {
+	grpc.ServerStream
+}
+
+func (x *citiesServiceGetCitiesServer) Send(m *CitiesStream) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func _CitiesService_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CityInput)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CitiesServiceServer).Create(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CitiesService_Create_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CitiesServiceServer).Create(ctx, req.(*CityInput))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CitiesService_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(City)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CitiesServiceServer).Update(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CitiesService_Update_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CitiesServiceServer).Update(ctx, req.(*City))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CitiesService_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Id)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CitiesServiceServer).Delete(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CitiesService_Delete_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CitiesServiceServer).Delete(ctx, req.(*Id))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -102,7 +264,25 @@ var CitiesService_ServiceDesc = grpc.ServiceDesc{
 			MethodName: "GetCity",
 			Handler:    _CitiesService_GetCity_Handler,
 		},
+		{
+			MethodName: "Create",
+			Handler:    _CitiesService_Create_Handler,
+		},
+		{
+			MethodName: "Update",
+			Handler:    _CitiesService_Update_Handler,
+		},
+		{
+			MethodName: "Delete",
+			Handler:    _CitiesService_Delete_Handler,
+		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "GetCities",
+			Handler:       _CitiesService_GetCities_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "city_service.proto",
 }

2a867c4bdbafd0f6133c3bdc3c54f09af5d706d3
m2k6artworks <zidangibran14@gmail.com>
2024-07-07 23:05:06 +0700

    [FEAT] create city

--- models/city.go
+++ models/city.go
@@ -11,12 +11,28 @@ type City struct {
 }
 
 func (u *City) Get(ctx context.Context, db *sql.DB, in *cities.Id) error {
-	u.Pb.Id = 1
-	u.Pb.Name = "Denpasar"
 	query := `SELECT id, name FROM cities WHERE id = $1`
 	err := db.QueryRowContext(ctx, query, in.Id).Scan(&u.Pb.Id, &u.Pb.Name)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (u *City) Create(ctx context.Context, db *sql.DB, in *cities.CityInput) error {
+	query := `INSERT INTO cities (name) VALUES ($1) RETURNING id;`
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	err = stmt.QueryRowContext(ctx, in.Name).Scan(&u.Pb.Id)
+	if err != nil {
+		return err
+	}
+
+	u.Pb.Name = in.Name
+
+	return nil
+
 }
